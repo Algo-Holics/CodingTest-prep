d4920fff00afb1f4f9b69ecef69d537a13a73df4
Jamie Lee <itzjamie96@gmail.com>
2021-01-29 22:15:43 +0900

    [jamie] DP modified

--- practice/dp/1로 만들기.py
+++ practice/dp/1로 만들기.py
@@ -4,8 +4,10 @@ start = time.time()
 x = int(input())
 d = [0] * (x+1)
 
+# bottom-up : 2~끝까지
+# top-down : 끝에서 작은 수까지
 for i in range(2, x+1):
-    d[i] = d[i-1]+1
+    d[i] = d[i-1]+1     # 초기값을 위해서 
     if i%2 == 0:
         d[i] = min(d[i], d[i//2]+1)
     if i%3 == 0:
